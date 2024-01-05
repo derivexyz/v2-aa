13136d477ddc28902f137e286f5c4df40433dbbe
Anton Cheng <antonassocareer@gmail.com>
2024-01-05 10:26:56 +0800

    build: compile

--- script/deploy-forwarder.s.sol
+++ script/deploy-forwarder.s.sol
@@ -25,19 +25,17 @@ contract Deploy is Script {
         DeploymentConfig memory config = _getConfig();
 
         // deploy LyraSponsoredForwarder
-        LyraSponsoredForwarder sponsoredForwarder = new LyraSponsoredForwarder{value: config.fundingAmount}(
-            config.usdcLocal,
-            config.socketVault
-        );
+        LyraSponsoredForwarder sponsoredForwarder =
+            new LyraSponsoredForwarder{value: config.fundingAmount}(config.usdcLocal, config.socketVault);
 
-        LyraSelfPayingForwarder selfPayingForwarder = new LyraSelfPayingForwarder{value: config.fundingAmount}(
-            config.usdcLocal,
-            config.socketVault
-        );
+        // LyraSelfPayingForwarder selfPayingForwarder = new LyraSelfPayingForwarder{value: config.fundingAmount}(
+        //     config.usdcLocal,
+        //     config.socketVault
+        // );
 
         console2.log("LyraSponsoredForwarder deployed at: ", address(sponsoredForwarder));
 
-        console2.log("LyraSelfPayingForwarder deployed at: ", address(selfPayingForwarder));
+        // console2.log("LyraSelfPayingForwarder deployed at: ", address(selfPayingForwarder));
 
         vm.stopBroadcast();
     }
@@ -46,23 +44,21 @@ contract Deploy is Script {
         if (block.chainid == 420) {
             // OP-Goerli
             return DeploymentConfig({
-                fundingAmount: 0.15 ether,
+                fundingAmount: 0.07 ether,
                 usdcLocal: 0x0f8BEaf58d4A237C88c9ed99D82003ab5c252c26, // our clone of USDC on op-goerli
                 // Socket configs
                 // See: https://github.com/SocketDotTech/app-chain-token/blob/lyra-tesnet-to-prod/deployments/prod_lyra_addresses.json
                 socketVault: 0x3d74c019E9caCBc968cF31B0810044a030B3E903
-            })
+            });
             // socketConnector: 0xfBf496B6DBda9d5e778e2563493BCb32F5A52B51
-            ;
         } else if (block.chainid == 1) {
             // Mainnet
             return DeploymentConfig({
                 fundingAmount: 0.15 ether,
                 usdcLocal: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, // mainnet USDC
                 socketVault: 0x6D303CEE7959f814042D31E0624fB88Ec6fbcC1d
-            })
+            });
             // socketConnector: 0x0000000000000000000000000000000000000001 // todo: add l1 address
-            ;
         }
 
         revert("No config for this network! Please set config in script/Deploy.s.sol");
---
+++ src/gelato/LyraPermitForwarder.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {GelatoRelayContextERC2771} from "../../lib/relay-context-contracts/contracts/GelatoRelayContextERC2771.sol";
+
+import {ISocketVault} from "../interfaces/ISocketVault.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {ILightAccountFactory} from "../interfaces/ILightAccountFactory.sol";
+
+/**
+ * @title  LyraPermitBridgeForwarder
+ * @notice Use this contract to allow gasless transactions, users pay gelato relayers in USDC
+ *
+ * @dev    All functions are guarded with onlyGelatoRelayERC2771. They should only be called by GELATO_RELAY_ERC2771 or GELATO_RELAY_CONCURRENT_ERC2771
+ * @dev    Someone need to fund this contract with ETH to use Socket Bridge
+ */
+contract LyraPermitBridgeForwarder is Ownable, GelatoRelayContextERC2771 {
+    ///@dev SocketVault address.
+    address public immutable socketVault;
+
+    ///@dev local token address. This token must support permit
+    address public immutable usdcLocal;
+
+    ///@dev Light Account factory address.
+    ///     See this script for more info https://github.com/alchemyplatform/light-account/blob/main/script/Deploy_LightAccountFactory.s.sol
+    address public constant lightAccountFactory = 0x000000893A26168158fbeaDD9335Be5bC96592E2;
+
+    struct PermitData {
+        uint256 value;
+        uint256 deadline;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
+    constructor(address _usdcLocal, address _socketVault) payable GelatoRelayContextERC2771() {
+        usdcLocal = _usdcLocal;
+        socketVault = _socketVault;
+    }
+
+    /**
+     * @notice  Deposit USDC to L2 through socket bridge. Gas is paid in token
+     * @dev     Users never have to approve USDC to this contract.
+     * @param maxFeeToken   Maximum fee in that user is willing to pay
+     * @param isScwWallet   True if user wants to deposit to default LightAccount on L2. False if the user wants to deposit to its own L2 address
+     * @param minGasLimit   Minimum gas limit for the L2 execution
+     * @param connector     Socket Connector
+     * @param permitData   Data and signatures for permit
+     */
+    function depositGasless(
+        uint256 maxFeeToken,
+        bool isScwWallet,
+        uint32 minGasLimit,
+        address connector,
+        PermitData calldata permitData
+    ) external payable onlyGelatoRelayERC2771 {
+        address msgSender = _getMsgSender();
+
+        try IERC20Permit(usdcLocal).permit(
+            msgSender, address(this), permitData.value, permitData.deadline, permitData.v, permitData.r, permitData.s
+        ) {} catch {}
+
+        IERC20(usdcLocal).transferFrom(msgSender, address(this), permitData.value);
+
+        // Pay gelato fee, reverts if exceeded max fee
+        _transferRelayFeeCapped(maxFeeToken);
+
+        uint256 remaining = permitData.value - _getFee();
+
+        uint256 socketFee = ISocketVault(socketVault).getMinFees(connector, minGasLimit);
+
+        // Pay socket fee and deposit to Lyra Chain
+        ISocketVault(socketVault).depositToAppChain{value: socketFee}(
+            _getL2Receiver(msgSender, isScwWallet), remaining, minGasLimit, connector
+        );
+    }
+
+    /**
+     * @notice Return the receiver address on L2
+     */
+    function _getL2Receiver(address msgSender, bool isScwWallet) internal view returns (address) {
+        if (isScwWallet) {
+            return ILightAccountFactory(lightAccountFactory).getAddress(msgSender, 0);
+        } else {
+            return msgSender;
+        }
+    }
+
+    /**
+     * @dev  Owner can withdraw ETH deposited to cover socket protocol fee
+     */
+    function withdrawETH() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+}
