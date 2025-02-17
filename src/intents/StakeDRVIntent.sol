db7e59452193da5c50191f4880642fafc68cfe29
antoncoding <antonassocareer@gmail.com>
2025-02-17 10:56:20 +0800

    feat: stake intent

---
+++ src/intents/StakeDRVIntent.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IStakedDRV} from "../interfaces/derive/IStakedDRV.sol";
+
+import {IntentExecutorBase} from "./IntentExecutorBase.sol";
+
+/**
+ * @title  StakeDRVIntent
+ * @notice A shared contract that allows executor to help users auto stake DRV from smart wallet
+ * @dev    Users who wish to have the auto-stake feature need to approve this contract to spend their DRV
+ */
+contract StakeDRVIntent is IntentExecutorBase {
+    address public immutable DRV;
+    address public immutable StakedDRV;
+
+    event IntentStakeDRV(address indexed scw, uint256 amount);
+
+    constructor(address _drv, address _stakedDRV) {
+        DRV = _drv;
+        StakedDRV = _stakedDRV;
+    }
+
+    /**
+     * @notice Execute a stake intent to auto stake DRV.
+     * @dev    The SCW must have approved this contract to spend the token.
+     * @param scw The light account address
+     * @param amount The amount of DRV to stake
+     */
+    function executeStakeDRVIntent(address scw, uint256 amount) external onlyIntentExecutor {
+        IERC20(DRV).transferFrom(scw, address(this), amount);
+        IERC20(DRV).approve(address(StakedDRV), amount);
+
+        IStakedDRV(StakedDRV).convertTo(amount, scw);
+
+        emit IntentStakeDRV(scw, amount);
+    }
+}
---
+++ test/fork-tests/intent/StakeDRVIntent.t.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+// solhint-disable contract-name-camelcase
+// solhint-disable func-name-mixedcase
+
+pragma solidity ^0.8.18;
+
+import {Test} from "lib/forge-std/src/Test.sol";
+
+import {StakeDRVIntent} from "src/intents/StakeDRVIntent.sol";
+import {IERC20} from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IStakedDRV} from "src/interfaces/derive/IStakedDRV.sol";
+import {ILightAccount} from "src/interfaces/ILightAccount.sol";
+
+/**
+ * forge test --fork-url https://rpc.lyra.finance -vvv
+ */
+contract FORK_LYRA_StakeDRVIntent is Test {
+    address public drv = address(0x2EE0fd70756EDC663AcC9676658A1497C247693A);
+    address public stakedDRV = address(0x7499d654422023a407d92e1D83D387d81BC68De1);
+
+    // Mock scws
+    address public scw = address(0xa0a);
+
+    StakeDRVIntent public stakeIntent;
+    address public executor = address(0xb0b);
+
+    /**
+     * Only run the test when running with --fork flag, and connected to Lyra mainnet
+     */
+    modifier onlyDeriveMainnet() {
+        if (block.chainid != 957) return;
+        _;
+    }
+
+    function setUp() public onlyDeriveMainnet {
+        stakeIntent = new StakeDRVIntent(drv, stakedDRV);
+
+        deal(drv, scw, 1000 ether);
+
+        // set executor as intent executor
+        stakeIntent.setIntentExecutor(executor, true);
+
+        // scw approves stakeIntent to spend drv
+        vm.prank(scw);
+        IERC20(drv).approve(address(stakeIntent), type(uint256).max);
+    }
+
+    function testStakeIntent_DRV() public onlyDeriveMainnet {
+        uint256 erc20BalanceBefore = IERC20(drv).balanceOf(scw);
+        uint256 stakedDRVBalanceBefore = IERC20(stakedDRV).balanceOf(scw);
+
+        vm.startPrank(executor);
+        stakeIntent.executeStakeDRVIntent(scw, 1 ether);
+        vm.stopPrank();
+
+        uint256 erc20BalanceAfter = IERC20(drv).balanceOf(scw);
+        assertEq(erc20BalanceAfter, erc20BalanceBefore - 1 ether);
+
+        uint256 stakedDRVBalanceAfter = IERC20(stakedDRV).balanceOf(scw);
+        assertEq(stakedDRVBalanceAfter, stakedDRVBalanceBefore + 1 ether);
+    }
+
+    receive() external payable {}
+}
